c505f605618e36985b78149b04ff54775477d6ed
Jye Lewis <jye@jyelewis.com>
2021-06-06 13:59:14 +1000

    Added eonin-integration.js module

---
+++ assets/eonin-integration.js
@@ -0,0 +1,11 @@
+// this module interacts with the Eonin server to pull down lesson objects
+
+(function() {
+    console.log("Hello world");
+    function fetchLesson(lessonId) {
+
+    }
+
+    // expose function globally
+    window.fetchLesson = fetchLesson;
+})();
